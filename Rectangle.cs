be6d6f864f6d08d4c5b97339e89c08a0925e21db
erik2305 <59746108+erik2305@users.noreply.github.com>
2022-02-08 22:43:18 -0500

    completed Rectangle class

---
+++ Rectangle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    class Rectangle
+    {
+        private int length;
+        private int width;
+
+        public Rectangle()
+        {
+            length = 1;
+            width = 1;
+        }
+
+        public Rectangle(int length, int width)
+        {
+            Console.Write("Input the length: ");
+            length = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input the width: ");
+            width = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
